88bb525258d159314eb5b8ddd97f2a5b8b8c1cab
Oskar Dahlberg <oskar.dahlberg@gmail.com>
2015-04-14 05:49:31 +0200

    Added files

---
+++ javascripts/index.js
@@ -0,0 +1,21 @@
+$(function () {
+  $('#app').removeClass('loading');
+
+  $(window).on('scroll', function () {
+    $('#app').toggleClass('intro', $(window).scrollTop() == 0);
+    $('#app').toggleClass('reading', $(window).scrollTop() > 0);
+  });
+
+  $('a[href^="#"]').on('click', function (event) {
+    event.preventDefault();
+    $(this).blur();
+
+    var id = this.hash;
+    var element = $(id);
+    $('html, body').stop().animate({
+      scrollTop: element.length > 0 ? element.offset().top : 0
+    }, 250, 'swing', function () {
+      window.location.hash = id;
+    });
+  });
+});
